79e354431c8cc525ab675693f69a214e86fb75a5
Gourav Sharma <144099217+GouravSharma2003@users.noreply.github.com>
2023-11-22 09:34:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+    //typing text animation script
+    var typed = new Typed (".typing" , {
+        strings: ["Student", "Fresher", "Hardworker", "Web Developer", "Programmer","Coder"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+
+
+});
